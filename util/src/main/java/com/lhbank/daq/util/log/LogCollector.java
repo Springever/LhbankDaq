1816271a35c19a0d430571c4d1f6933c9e0b7def
Springever <945003577>
2018-07-23 16:10:46 +0800

    1.1.1-增加日志采集

--- util/src/main/java/com/lhbank/daq/util/log/LogCollector.java
+++ util/src/main/java/com/lhbank/daq/util/log/LogCollector.java
@@ -16,10 +16,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.Locale;
-import java.util.Map;
-
 /**
  * @author maowenping 2016-05-10
  * @description 日志收集主类
